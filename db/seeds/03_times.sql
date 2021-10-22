d5d61b8cc30541460f14c7fc7a0585353b88ea8a
RiazK <RiazCKhan@gmail.com>
2021-10-22 16:58:04 -0400

    Add: data

---
+++ db/seeds/03_times.sql
@@ -0,0 +1,5 @@
+INSERT INTO times (event_id, start_date, end_date, start_time, end_time) VALUES (1, '2022-10-04', '2022-10-05', '09:00', '10:00');
+INSERT INTO times (event_id, start_date, end_date, start_time, end_time) VALUES (1, '2022-11-10', '2022-11-11', '12:00', '13:00');
+INSERT INTO times (event_id, start_date, end_date, start_time, end_time) VALUES (2, '2021-12-20', '2021-12-25', '15:00', '16:00');
+INSERT INTO times (event_id, start_date, end_date) VALUES (4, '2021-11-01', '2021-11-03');
+INSERT INTO times (event_id, start_date, end_date, start_time, end_time) VALUES (5, '2021-12-19', '2021-12-23', '12:00', '18:00');
