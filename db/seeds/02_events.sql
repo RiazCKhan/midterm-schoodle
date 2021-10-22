38221a5abe444cdc648178a7c43180eb4bcbbe09
RiazK <RiazCKhan@gmail.com>
2021-10-22 16:27:45 -0400

    Add: data

---
+++ db/seeds/02_events.sql
@@ -0,0 +1,6 @@
+INSERT INTO events (owner_id, title) VALUES (1, 'Code Retreat');
+INSERT INTO events (owner_id, title) VALUES (2, 'Code Wars');
+INSERT INTO events (owner_id, title) VALUES (3, 'Cottage');
+INSERT INTO events (owner_id, title) VALUES (4, 'Family Reunion');
+INSERT INTO events (owner_id, title) VALUES (5, 'Surprise Birthday');
+INSERT INTO events (owner_id, title) VALUES (6, 'Coffee Date');
