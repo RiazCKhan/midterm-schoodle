ca3c49da788a7e034bbb2ca42279c8d651634986
RiazK <RiazCKhan@gmail.com>
2021-10-22 16:58:37 -0400

    Add: data to description

--- db/seeds/02_events.sql
+++ db/seeds/02_events.sql
@@ -1,6 +1,6 @@
-INSERT INTO events (owner_id, title) VALUES (1, 'Code Retreat');
-INSERT INTO events (owner_id, title) VALUES (2, 'Code Wars');
-INSERT INTO events (owner_id, title) VALUES (3, 'Cottage');
-INSERT INTO events (owner_id, title) VALUES (4, 'Family Reunion');
-INSERT INTO events (owner_id, title) VALUES (5, 'Surprise Birthday');
-INSERT INTO events (owner_id, title) VALUES (6, 'Coffee Date');
+INSERT INTO events (owner_id, title, description) VALUES (1, 'Code Retreat', 'A much needed holiday');
+INSERT INTO events (owner_id, title, description) VALUES (2, 'Code Wars', 'Lets GO!');
+INSERT INTO events (owner_id, title, description) VALUES (3, 'Cottage', 'Fishing Trip');
+INSERT INTO events (owner_id, title, description) VALUES (4, 'Family Reunion', 'Family Reunion 2021');
+INSERT INTO events (owner_id, title, description) VALUES (5, 'Surprise Birthday', 'Shhh its a secret');
+INSERT INTO events (owner_id, title, description) VALUES (6, 'Coffee Date', 'Long time no see');
