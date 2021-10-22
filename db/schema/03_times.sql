c77fc5396f65e45aa2c6296ca3fe2616854b4e82
RiazK <RiazCKhan@gmail.com>
2021-10-22 16:09:50 -0400

    Update: syntax error

--- db/schema/03_times.sql
+++ db/schema/03_times.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS times CASCADE;
 CREATE TABLE times (
   id SERIAL PRIMARY KEY NOT NULL,
-  event_id INTEGER REFERENCES event(id) ON DELETE CASCADE,
+  event_id INTEGER REFERENCES events(id) ON DELETE CASCADE,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
   start_time TIME,
