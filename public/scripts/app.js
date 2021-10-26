3cbd51fe1190fa44e30def8e86f26369d51c4eba
RiazK <RiazCKhan@gmail.com>
2021-10-26 00:13:48 -0400

    Complete POST request including all event details

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,98 @@
 // Client facing scripts here
 
-$(document).ready(function() {
+$(document).ready(function () {
   $("#copy").on("click", copy);
   $("#add-date-time-button").on("click", renderDates);
-
-})
+  $("#user-form").on("submit", sendTimes);
+});
 
 const copy = function () {
-  event.preventDefault()
-  let $button = $("#copy")
-  let $form = $button.closest("form")
-  let text = $form.find("#url-link").val()
-  navigator.clipboard.writeText(text)
-}
+  event.preventDefault();
+  let $button = $("#copy");
+  let $form = $button.closest("form");
+  let text = $form.find("#url-link").val();
+  navigator.clipboard.writeText(text);
+};
 
 const renderDates = function () {
-  event.preventDefault()
+  event.preventDefault();
 
-  const $form = $(this).closest("form")
+  const $form = $(this).closest("form");
 
-  let startDateDay = $form.find("#date-form__start-day").val()
-  let startDateMonth = $form.find("#date-form__start-month").val()
-  let startDateYear = $form.find("#date-form__start-year").val()
-  let startDateTime = $form.find("#date-form__start-time").val()
+  let startDateDay = $form.find("#date-form__start-day").val();
+  let startDateMonth = $form.find("#date-form__start-month").val();
+  let startDateYear = $form.find("#date-form__start-year").val();
+  let startDateTime = $form.find("#date-form__start-time").val();
 
-  let endDateDay = $form.find("#date-form__end-day").val()
-  let endDateMonth = $form.find("#date-form__end-month").val()
-  let endDateYear = $form.find("#date-form__end-year").val()
-  let endDateTime = $form.find("#date-form__end-time").val()
+  let endDateDay = $form.find("#date-form__end-day").val();
+  let endDateMonth = $form.find("#date-form__end-month").val();
+  let endDateYear = $form.find("#date-form__end-year").val();
+  let endDateTime = $form.find("#date-form__end-time").val();
 
-  let pollOption = `<div>
-  ${startDateTime} on ${startDateDay}/${startDateMonth}/${startDateYear} -
-  ${endDateTime} on ${endDateDay}/${endDateMonth}/${endDateYear}</div>`
+  // let $error = $form.find(".error-msg")
+  let $error = $("#date-form .error-msg")
 
-  $(".append-date-time").prepend(pollOption)
+  if (!startDateDay || !startDateMonth || !startDateYear || !startDateTime) {
+    $error
+      .text("Error: all fields required")
+      .slideDown("slow")
+      .delay(1500)
+      .slideUp("slow")
+    return false
+  }
 
-  // console.log('start', startDateDay, startDateMonth, startDateYear, startDateTime)
-  // console.log('end', endDateDay, endDateMonth, endDateYear, endDateTime)
+  if (!endDateDay || !endDateMonth || !endDateYear || !endDateTime) {
+    $error
+      .text("Error: all fields required")
+      .slideDown("slow")
+      .delay(1500)
+      .slideUp("slow")
+    return false
+  }
+
+  let pollOption = `
+    <div class='time-container'>
+    <span class="start-time"> ${startDateYear} / ${startDateMonth} / ${startDateDay} ${startDateTime} </span> -
+    <span class="end-time"> ${endDateYear} / ${endDateMonth} / ${endDateDay} ${endDateTime} </span>
+    </div>
+  `
+
+  $(".append-date-time").prepend(pollOption);
 
   $form.trigger("reset");
+}
+
+const sendTimes = function (allTimes) {
+  let allDates = $(".append-date-time");
+
+  let name = $("#user-form #name").val(); // DO NOT DELETE - Note: Find content by referencing 'double id / class'
+  let email = $("#user-form #email").val();
+  let title = $("#user-form #title").val();
+  let description = $("#user-form #description").val();
+
+  let data = {
+    name,
+    email,
+    title,
+    description,
+    startDates: [],
+    endDates: []
+  };
+
+  allDates.children('.time-container').each(function () {
+    let startTime = $(this).find(".start-time").text();
+    let endTime = $(this).find(".end-time").text();
+
+    data.startDates = startTime;
+    data.endDates = endTime;
+  });
+
+  $.ajax({
+    url: "/events/new",
+    type: "POST",
+    data: data
+    // success: success,
+    // dataType: dataType
+  });
+
 }
