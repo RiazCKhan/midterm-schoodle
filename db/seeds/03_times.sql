3e54c6e75d8f63cc2304f88075217b1a1831ead3
RiazK <RiazCKhan@gmail.com>
2021-10-25 20:10:18 -0400

    Update: seed information to reflect timestamp

--- db/seeds/03_times.sql
+++ db/seeds/03_times.sql
@@ -1,5 +1,5 @@
-INSERT INTO times (event_id, start_date, end_date, start_time, end_time) VALUES (1, '2022-10-04', '2022-10-05', '09:00', '10:00');
-INSERT INTO times (event_id, start_date, end_date, start_time, end_time) VALUES (1, '2022-11-10', '2022-11-11', '12:00', '13:00');
-INSERT INTO times (event_id, start_date, end_date, start_time, end_time) VALUES (2, '2021-12-20', '2021-12-25', '15:00', '16:00');
+INSERT INTO times (event_id, start_date, end_date) VALUES (1, '2022-10-04 09:00', '2022-10-05 10:00');
+INSERT INTO times (event_id, start_date, end_date) VALUES (1, '2022-11-10 12:00', '2022-11-11 13:00');
+INSERT INTO times (event_id, start_date, end_date) VALUES (2, '2021-12-20 15:00', '2021-12-25 16:00');
 INSERT INTO times (event_id, start_date, end_date) VALUES (4, '2021-11-01', '2021-11-03');
-INSERT INTO times (event_id, start_date, end_date, start_time, end_time) VALUES (5, '2021-12-19', '2021-12-23', '12:00', '18:00');
+INSERT INTO times (event_id, start_date, end_date) VALUES (5, '2021-12-19 12:00', '2021-12-23 18:00');
