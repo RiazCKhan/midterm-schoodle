554eb96b42a9502ce34cc3c04c0d5d1b20e2e2f2
RiazK <RiazCKhan@gmail.com>
2021-10-28 19:14:23 -0400

    Added ajax post

--- routes/vote.js
+++ routes/vote.js
@@ -7,20 +7,33 @@ module.exports = () => {
   router.get("/:uniqueUrl", (req, res) => {
     let uniqueUrl = req.params.uniqueUrl;
 
+    // cookie session - check
+    // if exist give access
+    // otherwise alternative render
+
     database.getEventByUrl(uniqueUrl)
     .then(event => {
-      console.log('vote.js', event)
+     // console.log('vote.js', event)
       res.render("votePoll", {event})
       console.log("success!");
     })
   });
 
+  router.post("/:uniqueUrl", (req, res) => {
+  // grab name and email req.body
 
-  router.post("/uniqueid", (req, res) => {
+  // without ajax will be roload
+
+  // console.log(req.body)
+
+ let uniqueUrl = req.body.url
+ // insert users table :: returning
 
 
 
 
-  })
+  // insert into votes table
+  res.json({url: `/vote/${uniqueUrl}`})
+})
   return router;
 };
