2b4a6b618f319549220d13bf946a09335693a1bf
Gareth <gar.carey@gmail.com>
2021-10-29 10:55:10 -0400

    add cookie session

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const sassMiddleware = require("./lib/sass-middleware");
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cookieSession = require('cookie-session');
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -20,6 +21,14 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
+
+//cookie session set up
+app.set('trust proxy', 1)
+
+app.use(cookieSession({
+  name:'session',
+  keys: ['key1', 'key2']
+}));
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
