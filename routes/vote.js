417608558f51c307d8e1a8a61448db5d5c07bf88
RiazK <RiazCKhan@gmail.com>
2022-04-01 16:50:36 -0400

    remove commented code for clarity

--- routes/vote.js
+++ routes/vote.js
@@ -7,29 +7,11 @@ module.exports = () => {
     let uniqueUrl = req.params.uniqueUrl;
     const email = req.session.email;
     const name = req.session.name;
-    // console.log("cookies 123", email, name);
 
-    // let timeId = [];
-    // let yesVotes;
-    // let noVotes;
-
-    // database.countYesVotes(uniqueUrl, timeId).then((result) => {
-    //   yesVotes = result;
-    //   console.log("count yes", result);
-    //   return yesVotes;
-    // });
-    // database.countNoVotes(uniqueUrl, timeId).then((result) => {
-    //   noVotes = result;
-    //   console.log("count no", result);
-    //   return noVotes;
-    // });
-
-    // console.log("vote counts", yesVotes, noVotes);
-    // console.log("timeId?", timeId);
     const formattedEvent = { times: {} };
 
     const times = await database.getTimeIdsByUrl(uniqueUrl);
-    console.log("times from function", times);
+    // console.log("times from function", times);
     times.forEach((time) => {
       formattedEvent.times[time.id] = {
         votes: [],
@@ -38,22 +20,7 @@ module.exports = () => {
       };
     });
 
-    /* let timeIdsResult;
-    const timeIdsArray = [];
-    await database.getTimeIdsByUrl(uniqueUrl).then((event) => {
-      console.log("time Ids inside?", event);
-      timeIdsResult = event;
-    });
-    console.log("time Ids outside", timeIdsResult);
-
-    timeIdsResult.forEach((data) => {
-      timeIdsArray.push(Object.values(data)[0]);
-    });
-
-    console.log("timeIdsArray", timeIdsArray); */
-
     const votes = await database.getVotesByUniqueUrl(uniqueUrl);
-    console.log("our new votes function", votes);
 
     votes.forEach((vote) => {
       if (!formattedEvent.title) {
@@ -70,19 +37,9 @@ module.exports = () => {
       formattedEvent.times[vote.time_id].votes.push(vote);
     });
 
-    console.log("fromatted event:", formattedEvent);
-
-    //await database.countYesVotes(uniqueUrl, timeIdsArray).then((result) => {
-    //console.log("Yes count", result);
-    //});
-
-    //await database.countNoVotes(uniqueUrl, timeIdsArray).then((result) => {
-    //console.log("No count", result);
-    //});
-
     database.getEventByUrl(uniqueUrl).then((event) => {
       // Take JS from votePoll.ejs AND set EJS variables HERE - THEN pass all across like event, name, email
-      console.log("vote.js", event);
+      // console.log("vote.js", event);
       res.render("votePoll", { event, name, email, formattedEvent });
       console.log("success!");
     });
@@ -96,11 +53,12 @@ module.exports = () => {
 
     req.session.email = voterInfo.email;
     req.session.name = voterInfo.name;
-
-    console.log("VOTEJS voter", voterInfo);
+    // console.log("VOTEJS voter", voterInfo);
 
     let timeId = [];
     let selection = [];
+
+    console.log('option1', req.body.optionOneVote)
 
     timeId.push(req.body.optionOneVote.split(",")[0]);
     timeId.push(req.body.optionTwoVote.split(",")[0]);
@@ -115,15 +73,13 @@ module.exports = () => {
       selection,
     };
 
-    console.log("timeId: ", timeId);
-    console.log("selection: ", selection);
+    // console.log("timeId: ", timeId);
+    // console.log("selection: ", selection);
 
     let uniqueUrl = req.body.url;
 
-    // console.log('what am i', database.getVotesFromEmail(voterInfo))
-
     const ifVotes = await database.getVotesFromEmail(voterInfo);
-    console.log("$$$$", ifVotes);
+    // console.log("$$$$", ifVotes);
 
     if (ifVotes && ifVotes.length) {
       await database.updateVotes(votes, voterInfo);
@@ -131,7 +87,6 @@ module.exports = () => {
       await database.addVotes(votes, voterInfo);
     }
 
-    // await database.addVotes(votes, voterInfo);
     res.json({ url: `/vote/${uniqueUrl}` });
   });
   return router;
